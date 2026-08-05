63ba10ce928616a1422fff31f4e783aa8c268243
Romz <v8tbmdm9rr@privaterelay.appleid.com>
2026-08-05 22:19:40 +0300

    add data/categories.ts

---
+++ data/categories.ts
@@ -0,0 +1,7 @@
+import type { Category } from "@/types/menu";
+
+export const CATEGORIES: Category[] = [
+  { id: "pizza", label: "Неаполитанская пицца" },
+  { id: "roman", label: "Римская пицца" },
+  { id: "drinks", label: "Напитки" },
+];
